702a171f45edd464257d22f337bf6111ed70ec71
goerkem.demirkol <goerkem.demirkol@bearingpoint.com>
2021-10-18 23:11:49 +0200

    npv payments added, chart added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-var riceData = {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets:
-        [
-            {
-                fillColor: "rgba(172,194,132,0.4)",
-                strokeColor: "#ACC26D",
-                pointColor: "#fff",
-                pointStrokeColor: "#9DB86D",
-                data: [203000, 15600, 99000, 25100, 30500, 24700]
-            }
-        ]
-}
-
-var rice = document.getElementById('rice');
-new     Chart(rice).Line(riceData);
 
 function execute() {
     var loanAmount = document.getElementById("loanAmount").value
@@ -22,15 +6,18 @@ function execute() {
     var paymentStart = new Date($("#paymentStart").val());
     var utilization = new Date($("#utilization").val());
     var maturity = new Date($("#maturity").val());
-    var payments = calculate(loanAmount, interestRate, frequency, utilization, maturity, paymentStart);
+    var paymentSchedule = calculate(loanAmount, interestRate, frequency, utilization, maturity, paymentStart);
     if (tableAlreadyFull()) {
         emptyTable();
     }
-    addPaymentIntoTable(payments);
 
-    var eir = xirr(payments) * 100;
-    var npv = paymentsNpv(payments,eir);
-    setNpvResult(npv,eir);
+    var result = document.getElementById("result");
+    result.className = "visible";
+    fillRepaymentTable(paymentSchedule);
+    var eir = xirr(paymentSchedule) * 100;
+    var npvs= paymentsNpv(paymentSchedule,eir);
+    setNpvResult(paymentSchedule, npvs, eir);
+
     
 }
 
@@ -40,45 +27,10 @@ function getElementInsideContainer(containerID, childID) {
     return (parent.id && parent.id === containerID) ? elm : {};
 }
 
-function addPaymentIntoTable(payments) {
-    var table = document.getElementById("resultTable");
-
-    for (let i = 0; i < payments.length; i++) {
-        var row = table.insertRow(i + 1);
-        var id = row.insertCell(0);
-        var previousPaymentDate = row.insertCell(1);
-        var paymentDate = row.insertCell(2);
-        var principalPayment = row.insertCell(3);
-        var accumulatingDays = row.insertCell(4);
-        var remaningPrincipal = row.insertCell(5);
-        var interestAmount = row.insertCell(6);
-        row.id = "payment-" + payments[i].id;
-        row.className = "payment";
-        id.innerHTML = payments[i].id;
-        id.className = "paymentId";
-        previousPaymentDate.innerHTML = convertDateToString(payments[i].previousPaymentDate);
-        previousPaymentDate.className = "previousPaymentDate";
-        paymentDate.innerHTML = convertDateToString(payments[i].paymentDate);
-        paymentDate.className = "paymentDate";
-        principalPayment.innerHTML = formatNumber(payments[i].principalPayment);
-        principalPayment.className = "principalPayment";
-        accumulatingDays.innerHTML = getDifferenceInDays(payments[i].paymentDate, payments[i].previousPaymentDate);
-        accumulatingDays.className = "accumulatingDays";
-        remaningPrincipal.innerHTML = formatNumber(payments[i].remainingPrincipal);
-        remaningPrincipal.className = "remaningPrincipal";
-        interestAmount.innerHTML = formatNumber(payments[i].interestAmount);
-        interestAmount.className = "interestAmount";
-
-    }
-    var result = document.getElementById("result");
-    result.className="visible";
-}
-
-
 function isValidPaymentStartDate(paymentStart, utilization) {
     if (isValidDate(paymentStart)) {
-        let paymentStart = new Date(paymentStart);
-        let utilization = new Date(utilization);
+        var paymentStart = new Date(paymentStart);
+        var utilization = new Date(utilization);
         if (paymentStart > utilization) {
             return false;
             console.log('Payment start date can not be earlier than utilization date!')
@@ -237,12 +189,55 @@ function emptyTable() {
     for (var i = 1; table.rows.length - 1; i++) {
         table.deleteRow(1);
     }
+    var tableNpv = document.getElementById("npvTable");
+    for (var i = 1; tableNpv.rows.length - 1; i++) {
+        tableNpv.deleteRow(1);
+    }
+    document.getElementById("npvP").innerHTML = "";
+    var chart = document.getElementById("npvChart").getContext("2d");
+    if (chart.className == "chartjs-render-monitor") {
+        chart.destroy();
+    }
 }
 
-function excelReport() {
+
+function fillRepaymentTable(payments) {
+    var table = document.getElementById("resultTable");
+
+    for (let i = 0; i < payments.length; i++) {
+        var row = table.insertRow(i + 1);
+        var id = row.insertCell(0);
+        var previousPaymentDate = row.insertCell(1);
+        var paymentDate = row.insertCell(2);
+        var principalPayment = row.insertCell(3);
+        var accumulatingDays = row.insertCell(4);
+        var remaningPrincipal = row.insertCell(5);
+        var interestAmount = row.insertCell(6);
+        row.id = "payment-" + payments[i].id;
+        row.className = "payment";
+        id.innerHTML = payments[i].id;
+        id.className = "paymentId";
+        previousPaymentDate.innerHTML = convertDateToString(payments[i].previousPaymentDate);
+        previousPaymentDate.className = "previousPaymentDate";
+        paymentDate.innerHTML = convertDateToString(payments[i].paymentDate);
+        paymentDate.className = "paymentDate";
+        principalPayment.innerHTML = formatNumber(payments[i].principalPayment);
+        principalPayment.className = "principalPayment";
+        accumulatingDays.innerHTML = getDifferenceInDays(payments[i].paymentDate, payments[i].previousPaymentDate);
+        accumulatingDays.className = "accumulatingDays";
+        remaningPrincipal.innerHTML = formatNumber(payments[i].remainingPrincipal);
+        remaningPrincipal.className = "remaningPrincipal";
+        interestAmount.innerHTML = formatNumber(payments[i].interestAmount);
+        interestAmount.className = "interestAmount";
+
+    }
+}
+
+
+function excelReport(tableName) {
     var tab_text = "<table border='2px'><tr bgcolor='#87AFC6'>";
     var textRange; var j = 0;
-    tab = document.getElementById('resultTable');
+    tab = document.getElementById(tableName);
 
     for (j = 0; j < tab.rows.length; j++) {
         tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
@@ -268,34 +263,7 @@ function excelReport() {
 }
 
 
-
-function paymentsNpv(payments, eir) {
-    var totalNpv = 0;
-    try {
-        for (var i = 1; i < payments.length; i++) {
-            totalNpv += npv(payments[i].previousPaymentDate,
-                payments[i].paymentDate,
-                (payments[i].principalPayment + payments[i].interestAmount),
-                eir);
-            startDate = payments[i].paymentDate;
-        }
-        return totalNpv;
-    } catch (error) {
-        console.log(error);
-    }
-    return totalNpv;
-}
-
-function npv(startDate, paymentDate, cashflow, eir) {
-    var days = getDifferenceInDays(paymentDate, startDate);
-    if (days != 0) {
-        return cashflow / ((1 + eir / 100) ** (days / 365))
-    }
-    return cashflow;
-}
-
 function xirr(payments) {
-    // Credits: algorithm inspired by Apache OpenOffice
 
     // Calculates the resulting amount
     var irrResult = function (payments, rate) {
@@ -358,19 +326,11 @@ function xirr(payments) {
     return resultRate;
 }
 
-function testData() {
-    var loanAmount = document.getElementById("loanAmount");
-    loanAmount.value = 2500000;
-    var interestRate = document.getElementById("interestRate");
-    interestRate.value = 3;
-    var frequency = getElementInsideContainer("frequencyDropdownGr", "frequency");
-    frequency.value = "M";
-
-}
-
-function setNpvResult(npv, eir) {
-    document.getElementById("npvResult").innerHTML =
-        "Net present value of this loan is " + formatNumber(npv) +
+function setNpvResult(payments, paymentsNpv, eir) {
+    fillNpvTable(payments, paymentsNpv);
+    fillNpvChart();
+    document.getElementById("npvP").innerHTML =
+        "Net present value of this loan is " + formatNumber(getTotalNpv(paymentsNpv)) +
     " with eir of %" + formatNumber(eir, 4);
 }
 
@@ -379,4 +339,120 @@ function formatNumber(number, digits) {
         return number.toLocaleString(undefined, { minimumFractionDigits: 2 })
     }
     return number.toLocaleString(undefined, { minimumFractionDigits: digits })
+}
+
+function paymentsNpv(payments, eir) {
+    var npvs = [];
+    
+    try {
+        for (var i = 1; i < payments.length; i++) {
+            var npvTmp = npv(payments[i].previousPaymentDate,
+                payments[i].paymentDate,
+                (payments[i].principalPayment + payments[i].interestAmount),
+                eir);
+            npvs.push(new Payment(i-1, npvTmp, 0, 0, 0, null, payments[i].paymentDate));
+        }
+        return npvs;
+    } catch (error) {
+        console.log(error);
+    }
+    return paymentsNpv;
+}
+
+function npv(startDate, paymentDate, cashflow, eir) {
+    var days = getDifferenceInDays(paymentDate, startDate);
+    if (days != 0) {
+        return cashflow / ((1 + eir / 100) ** (days / 365))
+    }
+    return cashflow;
+}
+
+function getTotalNpv(paymentsNpv) {
+    var totalNpv = 0;
+    for (var i = 0; i < paymentsNpv.length; i++) {
+        totalNpv += paymentsNpv[i].principalPayment; 
+    }
+    return totalNpv;
+}
+
+function fillNpvTable(payments, npvs) {
+    var table = document.getElementById("npvTable");
+    var totalCashFlowVal = 0;
+    var totalNpvVal = 0;
+    for (let i = 0; i < npvs.length; i++) {
+        var row = table.insertRow(i + 1);
+        var id = row.insertCell(0);
+        var paymentDate = row.insertCell(1);
+        var cashFlow= row.insertCell(2);
+        var npv = row.insertCell(3);
+        var totalCashFlow = row.insertCell(4);
+        var totalNpv = row.insertCell(5);
+        
+        row.id = "npv-" + npvs[i].id;
+        row.className = "npv";
+        id.innerHTML = npvs[i].id;
+        id.className = "npvId";
+        paymentDate.innerHTML = convertDateToString(npvs[i].paymentDate);
+        paymentDate.className = "paymentDate";
+        var curCashFlow = payments[i + 1].principalPayment + payments[i + 1].interestAmount
+        cashFlow.innerHTML = formatNumber(curCashFlow);
+        cashFlow.className = "cashFlow";
+        totalCashFlowVal += curCashFlow;
+        var curNpv = npvs[i].principalPayment
+        totalNpvVal += curNpv;
+        npv.innerHTML = formatNumber(curNpv);
+        npv.className = "npvAmount";
+        totalCashFlow.innerHTML = formatNumber(totalCashFlowVal);
+        totalCashFlow.className = "totalCashFlow";
+        totalNpv.innerHTML = formatNumber(totalNpvVal);
+        totalNpv.className = "totalNpv";
+
+    }
+}
+
+function fillNpvChart() {
+
+    var dates = getValuesInsideContainerFromClass("npvTable", "paymentDate", false);
+    var cashFlows = getValuesInsideContainerFromClass("npvTable","cashFlow", true);
+    var npvs = getValuesInsideContainerFromClass("npvTable", "npvAmount", true);
+
+    var ctxL = document.getElementById("npvChart").getContext('2d');
+    ctxL = new Chart(ctxL, {
+        type: 'bar',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: "Fiscal Cashflow",
+                data: cashFlows,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 0.2)'
+            },
+            {
+                label: "NPV of Cashflow",
+                data: npvs,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 0.2)'
+            }
+            ]
+        },
+        options: {
+            responsive: true
+        }
+    });
+}
+
+function getValuesInsideContainerFromClass(container, className,castToNumber) {
+    var npvTable = document.getElementById(container);
+    var elements = npvTable.getElementsByClassName(className);
+    var values = [];
+
+    for (var i = 0; i < elements.length; i++) {
+        if (castToNumber) {
+            values.push(Number(elements[i].innerHTML.replaceAll(",","")));
+        } else {
+            values.push(elements[i].innerHTML);
+        }
+    }
+    return values;
+
 }
